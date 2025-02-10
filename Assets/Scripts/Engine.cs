edaea342db52ccd51fa02c2dba4bd2bd3372a2af
Telleem1 <Tjungschlegerspam@gmail.com>
2025-02-10 15:47:12 +0000

    Update Engine

--- Assets/Scripts/Engine.cs
+++ Assets/Scripts/Engine.cs
@@ -4,13 +4,50 @@ public class Engine : MonoBehaviour
 {
     [SerializeField] public List<Player> players;
     [SerializeField] private int startingMoney = 1500;
-}
+    [SerializeField] private int passGoMoney = 200;
+    [SerializeField] private int maxPlayers = 5;
+    [SerializeField] private BoardManager boardmanager;
+    [SerializeField] private Dice dice;
+    
+    private int currentPlayerIndex = 0;
+    private bool gameOver = false;
 
-void Start
-{
 
-}
-private void GiveStartingMoney(Player player)
-{
-    player.AddMoney(startingMoney);
+    void Start
+    {
+        initializeGame();
+    }
+
+    private void initializeGame(Player player)
+    {
+        foreach (var player in players)
+        {
+            player.AddMoney(startingMoney);
+            Debug.Log($"{player.PLayerName} has {player.money} starting money");
+        }
+    }   
+    private void passGo(Player player)
+    {
+        Debug.Log($"{player.PlayerName} passed Go")
+        player.Addmoney(passGoMoney);
+    }
+    private void nextTurn()
+    {
+        Player currentPlayer = players[currentPlayerIndex];
+    }
+
+    private void RollAndMove(int diceValue, Player player)
+    {
+
+    }
+
+    private void transactProperty(Player player, Property property)
+    {
+
+    }
+
+    private void transactHouses(Player player, Property property, int houses)
+    {
+        
+    }
 }
