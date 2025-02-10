202e282aa25c0278412f4331320b1ff644ff3a72
beck314 <beck.ewins@gmail.com>
2025-02-10 14:51:45 +0000

    playerScript start
    adds a filler sprite for the iron and basic setup for the character

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int money = 1500;
+        int playerID;
+        Property protperties[];
+        Tile currentTile = passGo;
+        bool inJail = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
