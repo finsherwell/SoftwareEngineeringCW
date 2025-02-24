04140657f5ceccef61d1a93b0398e33bba28edd2
finsherwell <finleysherwell@icloud.com>
2025-02-24 12:53:48 +0000

    Property Implemented
    Partial/Mostly final implementation of the Property.cs class, awaiting any updates to dependent classes for final implementation.

--- Assets/Scripts/Property.cs
+++ Assets/Scripts/Property.cs
@@ -1,39 +1,78 @@
-/*using UnityEngine;
+using UnityEngine;
 
 public class Property : MonoBehaviour
 {
-    public Tile tile;
-    private int setID;
-    private int price;
-    private int rent;
-    private bool isOwned;
+    [SerializeField] private string propertyName;
+    [SerializeField] private string group; // Color set (e.g., "Brown", "Blue")
+    [SerializeField] private int price;
+    [SerializeField] private int houseCost;
+    [SerializeField] private int[] rentLevels; // Rent for different levels (0-4 houses + hotel)
+    
+    private bool isOwned = false;
+    private int houses = 0;
+    private Player owner = null;
 
-    // public bool isProperty()
-    // {
-    //     return tile.isProperty();
-    // }
-    public int getSet()
+    public string GetPropertyName()
     {
-        return this.setID;
+        return propertyName;
     }
-    public int getPrice()
+
+    public string GetGroup()
     {
-        return this.price;
+        return group;
     }
-    public int getRent()
+
+    public int GetPrice()
     {
-        return this.rent;
+        return price;
     }
-    // private bool isOwned()
-    // {
-    //     return this.isOwned;
-    // }
-    // public player OwnedBy()
-    // {
-    //     if (isOwned())
-    //     {
-    //         //return player that owns property 
-    //     }
-    // }
+
+    public int GetHouseCost()
+    {
+        return houseCost;
+    }
+
+    public int GetRent()
+    {
+        return rentLevels[houses];
+    }
+
+    public bool IsOwned()
+    {
+        return isOwned;
+    }
+
+    public Player GetOwner()
+    {
+        return owner;
+    }
+
+    public void PurchaseProperty(Player buyer)
+    {
+        if (!isOwned && buyer.money >= price)
+        {
+            buyer.money -= price;
+            owner = buyer;
+            isOwned = true;
+        }
+    }
+
+    public void UpgradeProperty()
+    {
+        if (isOwned && owner.money >= houseCost && houses < 5) // Max: 4 houses + hotel
+        {
+            owner.money -= houseCost;
+            houses++;
+        }
+    }
+
+    public void PayRent(Player tenant)
+    {
+        if (isOwned && tenant != owner)
+        {
+            int rent = GetRent();
+            tenant.money -= rent;
+            owner.money += rent;
+        }
+    }
 }
-*/
