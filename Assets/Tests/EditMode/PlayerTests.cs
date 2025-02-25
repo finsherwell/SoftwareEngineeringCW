4b538048cf0f76574cb8c06191fcd8ba57467558
beck314 <beck.ewins@gmail.com>
2025-02-25 17:42:30 +0000

    more tests
    deleted unused folder 'exampleTests' and fixed some test dependencies

--- Assets/Tests/EditMode/PlayerTests.cs
+++ Assets/Tests/EditMode/PlayerTests.cs
@@ -3,7 +3,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class NewTestScript
+public class PlayerTests
 {
     // A Test behaves as an ordinary method
     [Test]
@@ -20,6 +20,42 @@ public class NewTestScript
         tplayer.setID(5);
         Assert.AreEqual(tplayer.getID(), 5);
     }
+ 
+    [Test]
+    public void CanAddMoney()
+    {
+        Player tPlayer = new Player();
+        tPlayer.addMoney(100);
+        Assert.AreEqual(tPlayer.getMoney(), 100);
+    }
 
+    [Test]
+    public void CanLoseMoney()
+    {
+        Player tPlayer = new Player();
+        tPlayer.addMoney(200);
+        tPlayer.takeMoney(100);
+        Assert.AreEqual(tPlayer.getMoney(), 100);
+    }
+    
+    [Test]
+    public void NeverNegativeMoney()
+    {
+        Player tPlayer = new Player();
+        tPlayer.takeMoney(100);
+        Assert.AreEqual(tPlayer.getMoney(), 0);
+        //could be changed to check whether the player enters a bankrupt state
+    }
 
+    //will add tests for all the tile logic as well 
+
+    /*
+    public void StoodOnTile()
+    {
+        Tile testTile = new Tile();
+        Player tPlayer = new Player();
+        testTile.GetLandedOn(tPlayer);
+        Assert.AreEqual(tPlayer.getCurrentTile().GetID(), testTile.GetID());
+    }
+    */
 }
