2ec0492bfa0b9b994fff021969d2b823dda30664
Telleem1 <Tjungschlegerspam@gmail.com>
2025-02-27 16:47:31 +0000

    Scene save for player movement

--- Assets/Scripts/Engine.cs
+++ Assets/Scripts/Engine.cs
@@ -4,29 +4,33 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 
-
 public class Engine : MonoBehaviour
 {
     [SerializeField] public List<Player> players;
     [SerializeField] private int startingMoney = 1500;
     [SerializeField] private int passGoMoney = 200;
     [SerializeField] private int maxPlayers = 5;
+
+    [SerializeField] private BoardManager boardmanager;
+    [SerializeField] private Dice dice;
+
+    [SerializeField] private int currentPlayerIndex = 0;
+    [SerializeField] private TextMeshProUGUI currentPlayerText;
+    private bool gameOver = false;
     [SerializeField] private int playerCount = 0; 
-
-
     [SerializeField] public Dice dice1;
     [SerializeField] public Dice dice2;
-
-
-    [SerializeField] private TextMeshProUGUI currentPlayerText;
-
-
     [SerializeField] private Button rollButton;
     [SerializeField] private Button nextTurnButton;
 
     public Player currentPlayer;
-    private int currentPlayerIndex = 0;
-    private bool gameOver = false;
+
+    
+     public void passGo(Player player) //when player lands on Go, add money to their account
+     {
+         Debug.Log($"{player.playerName} passed Go");
+         player.addMoney(passGoMoney);
+     }
 
     private void Start()
     {
@@ -54,37 +58,64 @@ public class Engine : MonoBehaviour
                 // Calculate total dice value
                 int totalDiceValue = dice1Value + dice2Value;
                 Debug.Log($"Total Dice Value: {totalDiceValue}");
-
-                // Move the player based on the total dice value
                 movePlayer(totalDiceValue, currentPlayer);
+                // Move the player based on the total dice value
+                //movePlayer(totalDiceValue, currentPlayer);
                 Debug.Log($"Player has moved {totalDiceValue} positions.");
             });
         });
+        
         nextTurnButton.gameObject.SetActive(true);
+    }
+    private void FindPlayers()
+    {
+        GameObject [] playerObjects = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject playerObject in playerObjects)
+        {
+            Player player = playerObject.GetComponent<Player>();
+            if (player!= null && players.Count < maxPlayers)
+            {
+                players.Add(player);
+                playerCount++;
+            }
+        }
+        Debug.Log($"found and added {players.Count} players");
     }
 
 
-
-    private void initializeGame() // Create players, initialize money, and set starting position
+     private void initializeGame()
     {
-        int i = 0;
-        foreach (var player in players)
+        nextTurnButton.gameObject.SetActive(false);
+        FindPlayers();
+        Tile startTile = boardmanager.GetTile(0);
+        foreach (Player player in players)
         {
             player.addMoney(startingMoney);
             Debug.Log($"{player.playerName} has {player.money} starting money");
-            player.setID(i);
-            players[i] = player;
-            i++;
-            playerCount++;
+            player.setID(players.IndexOf(player));
+            player.setCurrentTile(startTile);
+            player.transform.position = startTile.transform.position;
+
         }
-        nextTurnButton.gameObject.SetActive(false);
-        rollButton.gameObject.SetActive(true);
+
+        if (players.Count > 0)
+        {
+            currentPlayer = players[0];
+            updateTurnText(currentPlayer);
+        }
+        else
+        {
+            Debug.LogError("No players found in the scene!");
+        }
+        
     }
 
-    public void passGo(Player player) // When player lands on Go, add money to their account
+ 
+
+    private void RollAndMove(int diceValue, Player player)
     {
-        Debug.Log($"{player.playerName} passed Go");
-        player.addMoney(passGoMoney);
+        nextTurnButton.gameObject.SetActive(false);
+        rollButton.gameObject.SetActive(true);
     }
 
     private void movePlayer(int diceValue, Player player)
@@ -122,13 +153,15 @@ public class Engine : MonoBehaviour
     public void nextTurn() // Increment current player index and wrap when maxxed
     {
         currentPlayerIndex++;
-        if (currentPlayerIndex == playerCount+1)
+        if (currentPlayerIndex == playerCount)
         {
             currentPlayerIndex = 0;
         }
         currentPlayer=players[currentPlayerIndex];
         nextTurnButton.gameObject.SetActive(false);
         rollButton.gameObject.SetActive(true);
+        updateTurnText(currentPlayer);
+
     }
     private void updateTurnText(Player player)
     {
