0b0291ab7983192dceb1fb0566afa779b2aa7b7e
Joseph N <joenew1289@gmail.com>
2025-04-06 18:01:48 +0100

    fixed bug
    wouldnt compile now game does dont think this script is used tho

--- Assets/Scripts/PlayerPanelManager.cs
+++ Assets/Scripts/PlayerPanelManager.cs
@@ -13,8 +13,32 @@ public class PlayerPanelManager : MonoBehaviour
         Image cardImage;
         foreach (var player in players)
         {
-            cardImage = playercards[player.getID()].GetComponent<Image>(); 
-            cardImage.color = player.colour;
+            cardImage = playercards[player.getID()].GetComponent<Image>();
+
+            switch (player.colour)
+            {
+
+                case MenuEnums.Colours.Green:
+                    if (ColorUtility.TryParseHtmlString("#33FF57", out var c)) cardImage.color = c;
+                    break;
+                case MenuEnums.Colours.Yellow:
+                    if (ColorUtility.TryParseHtmlString("#FFD700", out var d)) cardImage.color = d;
+                    break;
+                case MenuEnums.Colours.Blue:
+                    if (ColorUtility.TryParseHtmlString("#3357FF", out var e)) cardImage.color = e;
+                    break;
+                case MenuEnums.Colours.Purple:
+                    if (ColorUtility.TryParseHtmlString("#8A2BE2", out var f)) cardImage.color = f;
+                    break;
+                case MenuEnums.Colours.Red:
+                    if (ColorUtility.TryParseHtmlString("#FF5733", out var g)) cardImage.color = g;
+                    break;
+                case MenuEnums.Colours.Cyan:
+                    if (ColorUtility.TryParseHtmlString("#00FFFF", out var h)) cardImage.color = h;
+                    break;
+
+            }
+
             playerNameText[player.getID()].text = player.playerName;
         }
         for (int i = players.Count; i < playercards.Count; i++)
